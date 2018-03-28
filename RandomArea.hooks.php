1b42f9324a44345554fe4d372b8e533367a88ef4
Jayprakash12345 <0freerunning@gmail.com>
2018-03-29 02:01:53 +0530

    Convert RandomArea to use extension registration
    Bug: T190961
    Change-Id: Id937359044671744df6544ed5c9b7b8b3ac920a2

---
+++ RandomArea.hooks.php
@@ -0,0 +1,49 @@
+<?php
+
+class RandomAreaHooks {
+		public static function init( &$parser ) {
+				$parser->setHook( 'randomArea', [ 'RandomAreaHooks', 'renderRandomArea' ] );
+				return true;
+		}
+
+		static function renderRandomArea( $input, $argv, $parser ) {
+			$count = $argv['count'];
+			if ($count < 0 || !$count) {
+				$count = 1;
+			}
+			$nsPrefix = $argv['nsprefix'];
+			$include = (bool) $argv['include'];
+			$values = explode("\n", $input);
+			$valCount = count($values) - 1;
+			if ($valCount < $count) {
+				return $parser->internalParse("Exception:
+				<br />Array out of Bounce - >
+				Only ".$valCount." items available, count = ".$count );
+			}
+			$valueIndex = array();
+			$randOut = "";
+			$i = 0;
+			while ($i < $count ) {
+				$randVal = rand(0, $valCount);
+				$randTemp = trim($values[$randVal]);
+				if (strlen($randTemp) > 0 && !array_key_exists($randVal, $valueIndex)) {
+
+					if (strlen($nsPrefix) > 1) {
+						$randOutTemp = $nsPrefix.$randTemp;
+					} else {
+						$randOutTemp = $randTemp;
+					}
+
+					if ($include) {
+						$randOut .= "{{:".$randOutTemp."}}";
+					} else {
+						$randOut .= $randOutTemp;
+					}
+					$valueIndex[$randVal] = true;
+					$i++;
+				}
+
+			}
+			return $parser->internalParse($randOut);
+		}
+}
--- RandomArea.php
+++ RandomArea.php
@@ -9,71 +9,16 @@
  * @licence http://creativecommons.org/licenses/by-nc-sa/2.5/
  */
 
-/*
-* History:
-* 1.1: Fix loop if entry length < 1; Thanks to Steffen
-*
-*/
-
-if( !defined( 'MEDIAWIKI' ) ) {
-        echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
-        die( 1 );
-}
-
-define( 'RANDOM_AREA_VERSION', '1.1.1' );
-$wgExtensionFunctions[] = "ylRandomAreaExtension";
-
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Random Area',
-	'description'    => 'Allows for inserting random elements in a page',
-	'author'         => array( 'Thomas Stock' ),
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:RandomArea',
-	'version'        => RANDOM_AREA_VERSION,
-);
-
-function ylRandomAreaExtension() {
-    global $wgParser;
-    $wgParser->setHook( "randomArea", "renderRandomArea" );
-}
-
-function renderRandomArea( $input, $argv, $parser ) {
-	$count = $argv['count'];
-	if ($count < 0 || !$count) {
-		$count = 1;
-	}
-	$nsPrefix = $argv['nsprefix'];
-	$include = (bool) $argv['include'];
-	$values = explode("\n", $input);
-	$valCount = count($values) - 1;
-	if ($valCount < $count) {
-		return $parser->internalParse("Exception:
-		<br />Array out of Bounce - >
-		Only ".$valCount." items available, count = ".$count );
-	}
-	$valueIndex = array();
-	$randOut = "";
-	$i = 0;
-	while ($i < $count ) {
-		$randVal = rand(0, $valCount);
-		$randTemp = trim($values[$randVal]);
-		if (strlen($randTemp) > 0 && !array_key_exists($randVal, $valueIndex)) {
-
-			if (strlen($nsPrefix) > 1) {
-				$randOutTemp = $nsPrefix.$randTemp;
-			} else {
-				$randOutTemp = $randTemp;
-			}
-
-			if ($include) {
-				$randOut .= "{{:".$randOutTemp."}}";
-			} else {
-				$randOut .= $randOutTemp;
-			}
-			$valueIndex[$randVal] = true;
-			$i++;
-		}
-
-	}
-	return $parser->internalParse($randOut);
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'RandomArea' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['RandomArea'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the RandomArea extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the RandomArea extension requires MediaWiki 1.29+' );
 }
